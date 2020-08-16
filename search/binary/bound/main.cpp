1614d48322b80b89b5e5b090024c35404020c1c6
zzerross <zzerross@gmail.com>
2020-08-17 00:51:57 +0900

    algorithm: lower and upper bound was updated

--- search/binary/bound/main.cpp
+++ search/binary/bound/main.cpp
@@ -18,8 +18,7 @@ int* bnd(int* a, int l, int h, int t, bool u = false) {
 
 int main() {
 #define S 3
-
-    int A[S] = { 0, 5, 9 };
+    int A[S] = { 1, 4, 8 };
 
     for (int i = 0; i < S; i++)
         printf("%d ", A[i]);
@@ -29,32 +28,32 @@ int main() {
     printf("       Bound Bound\n");
 
     for (int i = 0; i < 10; i++) {
-        if (!(i % 5))
+        if (!(i % 4))
             printf("\n");
 
         printf("%6d ", i);
         printf("%2d/%2d ", *bnd(A, 0, S - 1, i, false), *lower_bound(A, A + S - 1, i));
         printf("%2d/%2d\n", *bnd(A, 0, S - 1, i, true), *upper_bound(A, A + S - 1, i));
-
     }
 
     return 0;
 }
 
 #ifdef output
-0 5 9 
+1 4 8 
 Target Lower Upper
        Bound Bound
 
-     0  0/ 0  5/ 5
-     1  5/ 5  5/ 5
-     2  5/ 5  5/ 5
-     3  5/ 5  5/ 5
-     4  5/ 5  5/ 5
+     0  1/ 1  1/ 1
+     1  1/ 1  4/ 4
+     2  4/ 4  4/ 4
+     3  4/ 4  4/ 4
 
-     5  5/ 5  9/ 9
-     6  9/ 9  9/ 9
-     7  9/ 9  9/ 9
-     8  9/ 9  9/ 9
-     9  9/ 9  9/ 9
+     4  4/ 4  8/ 8
+     5  8/ 8  8/ 8
+     6  8/ 8  8/ 8
+     7  8/ 8  8/ 8
+
+     8  8/ 8  8/ 8
+     9  8/ 8  8/ 8
 #endif
