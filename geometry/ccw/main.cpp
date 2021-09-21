24a3e9c8ab7b3051f1ee27e2bf3f4e2ab5478b89
kumhyun-cho <kumhyun.cho@samsung.com>
2021-09-22 00:37:34 +0900

    Vector2D.ccw() was updated

--- geometry/ccw/main.cpp
+++ geometry/ccw/main.cpp
@@ -1,49 +1,66 @@
 #include <stdio.h>
 
 template <typename T>
-struct vector2d {
+struct Vector2D {
     T x, y;
 
-    vector2d(T x, T y): x(x), y(y) {
+    Vector2D(): x(0), y(0) {
     }
 
-    T cross(const vector2d& o) const {
+    Vector2D(T x, T y): x(x), y(y) {
+    }
+
+    T cross(const Vector2D& o) const {
         return x * o.y - y * o.x;
     }
 
-    T ccw(const vector2d& o) const {
+    T ccw(const Vector2D& o) const {
         return cross(o);
+    }
+
+    T ccw(const Vector2D& a, const Vector2D& b) const {
+        Vector2D oa = a - *this;
+        Vector2D ob = b - *this;
+
+        return oa.cross(ob);
+    }
+
+    bool operator<(const Vector2D& o) const {
+        return x == o.x ?  y < o.y : x < o.x;
+    }
+
+    Vector2D operator-(const Vector2D& o) const {
+        return Vector2D<T>(x - o.x, y - o.y);
+    }
+
+    void dump(const char* s) {
+        pr1("%8s: %d %d\n", s, x, y);
     }
 };
 
 int main() {
-    {
-        vector2d<int> v(1, 1);
-        printf("\n"
-               " 2 b . . \n"
-               " 1 . a . \n"
-               " 0 + . . \n"
-               "   0 1 2 \n"
-               "ccw(a->b): %d\n", v.ccw({0, 2}));
-    }
-    {
-        vector2d<int> v(0, 2);
-        printf("\n"
-               " 2 a . . \n"
-               " 1 . b . \n"
-               " 0 + . . \n"
-               "   0 1 2 \n"
-               "ccw(a->b): %d\n", v.ccw({1, 1}));
-    }
-    {
-        vector2d<int> v(1, 1);
-        printf("\n"
-               " 2 . . b \n"
-               " 1 . a . \n"
-               " 0 + . . \n"
-               "   0 1 2 \n"
-               "ccw(a->b): %d\n", v.ccw({2, 2}));
-    }
+    Vector2D<int> o(0, 0);
+
+    printf("\n"
+            " 2 b . . \n"
+            " 1 . a . \n"
+            " 0 + . . \n"
+            "   0 1 2 \n"
+            "ccw(a->b): %d\n", o.ccw({1, 1}, {0, 2}));
+
+    printf("\n"
+            " 2 a . . \n"
+            " 1 . b . \n"
+            " 0 + . . \n"
+            "   0 1 2 \n"
+            "ccw(a->b): %d\n", o.ccw({0, 2}, {1, 1}));
+
+    printf("\n"
+            " 2 . . b \n"
+            " 1 . a . \n"
+            " 0 + . . \n"
+            "   0 1 2 \n"
+            "ccw(a->b): %d\n", o.ccw({1, 1}, {2, 2}));
 
     return 0;
 }
