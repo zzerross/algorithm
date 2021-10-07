6135258bfd46567475ae64e2daaa826c895951f0
kumhyun-cho <kumhyun.cho@samsung.com>
2021-10-08 02:57:37 +0900

    kmeans cpp implementation was updated with Array and Point2d structure

--- clustering/kmeans/cpp/main.cpp
+++ clustering/kmeans/cpp/main.cpp
@@ -10,69 +10,92 @@ using namespace std;
 #define pr0(fmt, ...)
 #endif
 
-#define DSTNC 0x07ffffff
+#define DISTANCE 0x07ffffff
 
-#define CLSTR_N 10
-int gClstrN;
-double gClstrX[CLSTR_N], gClstrY[CLSTR_N];
+template <typename T, int S>
+struct Array {
+    T buf[S];
+    int len;
 
-#define PNT_N 1000
-int gPntN;
-double gPntX[PNT_N], gPntY[PNT_N];
+    T& operator[](int i) {
+        return buf[i];
+    }
+};
 
-#define PNT_N 1000
-int gPntN;
-double gPntX[PNT_N], gPntY[PNT_N];
+template <typename T>
+struct Point2d {
+    T x, y;
 
-int nearest() {
-    for (int p = 0; p < gPntN; p++) {
-        gClstr[p] = 0;
+    void init() {
+        x = 0;
+        y = 0;
+    }
 
-        for (int m = DSTNC, c = 0; c < gClstrN; c++) {
-            int d = distance(gPntY[p], gPntX[p], gClstrY[c], gClstrX[c]);
-            if (d < gClstrD[p]) {
-                gClstrD[p] = d;
-                gClstr[p] = j;
+    T distance(const Point2d& o) const {
+        return 0;
+    }
+};
+
+template <int S>
+struct DoublePoint2dArray : public Array<Point2d<double>, S> {
+    void read() {
+        scanf("%d", &this->len);
+
+        for (int c = 0; c < this->len; c++)
+            scanf("%lf %lf", &this->buf[c].x, &this->buf[c].y);
+    }
+};
+
+#define CLUSTERS 10
+DoublePoint2dArray<CLUSTERS> gClusters;
+DoublePoint2dArray<CLUSTERS> gCenters;
+
+#define POINTS 1000
+DoublePoint2dArray<POINTS> gPoints;
+
+Array<double, POINTS> gDistanceOf;
+Array<int, POINTS> gClusterOf;
+
+void nearest() {
+    for (int p = 0; p < gPoints.len; p++) {
+        gClusterOf[p] = 0;
+
+        for (int m = DISTANCE, c = 0; c < gClusters.len; c++) {
+            int d = gPoints[p].distance(gClusters[c]);
+            if (d < gDistanceOf[p]) {
+                gDistanceOf[p] = d;
+                gClusterOf[p] = c;
             }
         }
     }
-
-    return 0;
 }
 
-int center() {
-    for (int c = 0; c < gClstrN; c++) {
-        gCntrN[c] = 0;
-        gCntrX[c] = gCntrY[c] = 0.0f;
-        for (int p = 0; p < gPntN; p++) {
-            if (c == gClstr[p]) {
-                gCntrX[c] = (gCntrX[c] + gPntX[p]) / (gClstrN[c] ? 2.0f : 1.0f);
-                gCntrY[c] = (gCntrY[c] + gPntY[p]) / (gClstrN[c] ? 2.0f : 1.0f);
-                gCntrN[c]++;
-            }
+void center() {
+    int points[CLUSTERS] = { 0, };
+
+    for (int c = 0; c < gClusters.len; c++) {
+        gCenters[c].init();
+
+        for (int p = 0; p < gPoints.len; p++) {
+            if (c != gClusterOf[p])
+                continue;
+
+            points[c]++;
+
+            gCenters[c].x = (gCenters[c].x + gPoints[p].x) / points[c];
+            gCenters[c].y = (gCenters[c].y + gPoints[p].y) / points[c];
         }
     }
 }
 
-int cluster() {
+int main() {
+    gClusters.read();
+    gPoints.read();
+
     while (true) {
         nearest();
         center();
     }
-
-    return 0;
-}
-
-int main() {
-    scanf("%d", &gClstrN);
-    for (int c = 0; c < gClstrN; c++)
-        scanf("%lf %lf", &gClstrX[c], &gClstrY[c]);
-
-    scanf("%d", &gPntN);
-    for (int p = 0; p < gPntN; p++)
-        scanf("%lf %lf", &gPntX[p], &gPntY[p]);
-
-    cluster();
 
     return 0;
 }
